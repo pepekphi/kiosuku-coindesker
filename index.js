27e22c4a4c0999e56bbc2e6adeb03af6056b75b0
diegodubai <dirkt82@hotmail.com>
2025-06-29 00:19:59 +0400

    Update index.js
    CHECK_INTERVAL_SECONDS = 10
    1 was hitting the rate limit, so let's try 10 now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const axios = require('axios');
 
 // ─── CONFIGURATION ────────────────────────────────────────────────────────────
-const CHECK_INTERVAL_SECONDS = 1; // interval in seconds (change as needed)
+const CHECK_INTERVAL_SECONDS = 10; // interval in seconds (change as needed)
 const SEND_ON_STARTUP = true; // If true, then it always sends the most recent article on startup. If it is false, then it will not do that, and wait for a new article from now on.
 const WEBHOOK_URL = 'https://kiosuku-production.up.railway.app/incoming';
 const COINDESK_API_URL = 'https://data-api.coindesk.com/news/v1/article/list?lang=EN&limit=1';
